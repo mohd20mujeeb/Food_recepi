dd236ccdea05c97d77a1a06c9ad084fc7a151c8b
mohd20mujeeb <mohd.20mujeeb@gmail.com>
2024-09-23 19:39:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Details from "./pages/details";
+import Home from "./pages/home";
+import Favorites from "./pages/favorites";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <div>
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg ">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/recipe-item/:id" element={<Details />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/favorites/index.jsx
@@ -0,0 +1,25 @@
+
+import RecipeItem from "../../components/navbar/recipe-item";
+import { GlobalContext } from "../../context";
+import { useContext } from "react";
+export default function Favorites()
+{
+
+   const {favoritesList} = useContext(GlobalContext);
+
+
+   return (
+     <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+       {favoritesList && favoritesList.length > 0 ? (
+         favoritesList.map((item) => <RecipeItem item={item} />)
+       ) : (
+         <div>
+           <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+             Nothing is added to favorites.
+           </p>
+         </div>
+       )}
+     </div>
+   );
+
+}
